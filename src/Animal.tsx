e67614d95f1647fdfbcf4b0274b5043e41dadc3c
Matt Miller <matt@m11r.dev>
2025-02-04 09:25:17 +0000

    feat: Using new useAnimalStats hook in Animal component

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,14 +1,15 @@
-import usePeriodicUpdate from "./hooks/usePeriodicUpdate";
+import useAnimalStats from "./hooks/useAnimalStats";
 import { AnimalType } from "./SharedTypes";
 
 export type AnimalProps = AnimalType & {};
 
 export const Animal = ({ name, breed, data }: AnimalProps) => {
-  const { timeSinceStarted } = usePeriodicUpdate(1000);
+  const { values, updateHunger, updateHappiness, updateSleepiness } =
+    useAnimalStats(data);
+
   return (
     <div className="animal-container">
       <h1>{breed}</h1>
-      <h2>{timeSinceStarted}</h2>
       <div className="animal-animal">
         <img
           src="./poodle.svg"
@@ -23,30 +24,39 @@ export const Animal = ({ name, breed, data }: AnimalProps) => {
           <div className="meter">
             <div
               className="meter-fill"
-              style={{ width: `${data.hunger}%` }}
+              style={{ width: `${values.hunger}%` }}
             ></div>
           </div>
-          <button className="action-button">Feed</button>
+          <button className="action-button" onClick={() => updateHunger(10)}>
+            Feed
+          </button>
         </div>
         <div className="stat">
           <strong>Happiness:</strong>
           <div className="meter">
             <div
               className="meter-fill"
-              style={{ width: `${data.happiness}%` }}
+              style={{ width: `${values.happiness}%` }}
             ></div>
           </div>
-          <button className="action-button">Play</button>
+          <button className="action-button" onClick={() => updateHappiness(10)}>
+            Play
+          </button>
         </div>
         <div className="stat">
           <strong>Sleep:</strong>
           <div className="meter">
             <div
               className="meter-fill"
-              style={{ width: `${data.sleepiness}%` }}
+              style={{ width: `${values.sleepiness}%` }}
             ></div>
           </div>
-          <button className="action-button">Rest</button>
+          <button
+            className="action-button"
+            onClick={() => updateSleepiness(10)}
+          >
+            Rest
+          </button>
         </div>
       </div>
     </div>
