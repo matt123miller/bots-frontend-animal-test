14d6dfc81d2f99f5ff13b7a2b0c774c3f33d33d5
Matt Miller <matt@m11r.dev>
2025-02-04 09:22:01 +0000

    feat: Created useAnimalStats This will update an animals stts over time and exposes 3 functions for updating the happiness, sleepiness and hunger.

--- src/hooks/useAnimalStats.test.ts
+++ src/hooks/useAnimalStats.test.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/vitest";
-import { renderHook } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { act, cleanup, renderHook } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
 import useAnimalStats from "./useAnimalStats";
 
 describe("useAnimalStats hook", () => {
@@ -9,12 +9,116 @@ describe("useAnimalStats hook", () => {
   const wait = (ms: number) =>
     new Promise((resolve) => setTimeout(resolve, ms));
 
-  it("temp test", () => {
-    const { result } = renderHook(() => useAnimalStats());
-    expect(result.current).toMatchObject({
-      hunger: 0,
-      happiness: 0,
-      sleepiness: 0,
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("Should initialize with the correct values, starting with the supplied initial values", () => {
+    const { result } = renderHook(() =>
+      useAnimalStats({ hunger: 10, happiness: 10, sleepiness: 10 }, 1000)
+    );
+    const { values, updateHunger, updateHappiness, updateSleepiness } =
+      result.current;
+
+    // The initial values are ticked by 1
+    expect(values).toMatchObject({
+      happiness: 9,
+      hunger: 11,
+      sleepiness: 11,
+    });
+  });
+
+  it("Should update the values correctly when state change functions are called", async () => {
+    const { result } = renderHook(() =>
+      useAnimalStats({ hunger: 10, happiness: 10, sleepiness: 10 }, 50)
+    );
+    const { updateHunger, updateHappiness, updateSleepiness } = result.current;
+
+    await act(() => {
+      updateHunger(10);
+      updateHappiness(10);
+      updateSleepiness(10);
+      return wait(10);
+    });
+
+    const { values } = result.current;
+
+    expect(values).toMatchObject({
+      happiness: 19,
+      hunger: 1,
+      sleepiness: 1,
+    });
+  });
+
+  it("Should update the values correctly over time", async () => {
+    const { result } = renderHook(() =>
+      useAnimalStats({ hunger: 10, happiness: 10, sleepiness: 10 }, 50)
+    );
+    const valuesBefore = result.current.values;
+
+    await act(() => wait(70));
+
+    const valuesAfter = result.current.values;
+
+    expect(valuesAfter).not.toMatchObject(valuesBefore);
+
+    expect(valuesAfter.happiness).toBeLessThan(valuesBefore.happiness);
+    expect(valuesAfter.hunger).toBeGreaterThan(valuesBefore.hunger);
+    expect(valuesAfter.sleepiness).toBeGreaterThan(valuesBefore.sleepiness);
+
+    expect(valuesAfter).toMatchObject({
+      happiness: 8,
+      hunger: 12,
+      sleepiness: 12,
+    });
+
+    await act(() => wait(70));
+
+    expect(result.current.values).toMatchObject({
+      happiness: 7,
+      hunger: 13,
+      sleepiness: 13,
+    });
+  });
+
+  it("Should update the happiness value at the increased rate when hunger and sleepiness are full", async () => {
+    const { result } = renderHook(() =>
+      useAnimalStats({ hunger: 98, happiness: 50, sleepiness: 98 }, 50)
+    );
+
+    // initial setup, 0th tick
+    expect(result.current.values).toMatchObject({
+      happiness: 49,
+      hunger: 99,
+      sleepiness: 99,
+    });
+
+    await act(() => wait(50));
+
+    // 1st tick
+    expect(result.current.values).toMatchObject({
+      happiness: 48,
+      hunger: 100,
+      sleepiness: 100,
+    });
+
+    await act(() => wait(50));
+
+    // 3rd tick
+    // Now hunger and sleepiness are full, subsequent ticks will decrease happiness at a higher rate
+    expect(result.current.values).toMatchObject({
+      happiness: 38,
+      hunger: 100,
+      sleepiness: 100,
+    });
+
+    await act(() => wait(50));
+
+    // 4th tick
+    expect(result.current.values).toMatchObject({
+      happiness: 28,
+      hunger: 100,
+      sleepiness: 100,
     });
   });
 });
--- src/hooks/useAnimalStats.ts
+++ src/hooks/useAnimalStats.ts
@@ -1,7 +1,103 @@
-export default function useAnimalStats() {
+import { useCallback, useEffect, useReducer } from "react";
+import { AnimalType } from "../SharedTypes";
+import usePeriodicUpdate from "./usePeriodicUpdate";
+
+type AnimalStats = AnimalType["data"];
+
+type Actions =
+  | {
+      type: "updateHunger";
+      amount: number;
+    }
+  | {
+      type: "updateHappiness";
+      amount: number;
+    }
+  | {
+      type: "updateSleepiness";
+      amount: number;
+    }
+  | {
+      type: "tick";
+      timeSinceStarted: number;
+    };
+
+const rateModifier = 1;
+const increasedRateModifier = 10;
+
+function reducer(state: AnimalStats, action: Actions) {
+  switch (action.type) {
+    case "tick":
+      const happinessModifier =
+        state.sleepiness === 100 || state.hunger === 100
+          ? increasedRateModifier
+          : rateModifier;
+      const tickedHappiness = Math.max(0, state.happiness - happinessModifier);
+
+      return {
+        happiness: tickedHappiness,
+        hunger: Math.min(100, state.hunger + rateModifier),
+        sleepiness: Math.min(100, state.sleepiness + rateModifier),
+      };
+    case "updateHunger":
+      const hunger = Math.min(100, state.hunger - action.amount);
+      return {
+        ...state,
+        hunger,
+      };
+    case "updateHappiness":
+      const happiness = Math.min(100, state.happiness + action.amount);
+      return {
+        ...state,
+        happiness,
+      };
+    case "updateSleepiness":
+      const sleepiness = Math.min(100, state.sleepiness - action.amount);
+      return {
+        ...state,
+        sleepiness,
+      };
+    default:
+      return state;
+  }
+}
+
+/**
+ *
+ * @param initialValues
+ * @param updateInterval Default is 1000ms
+ */
+export default function useAnimalStats(
+  initialValues: AnimalStats,
+  updateInterval: number = 1000
+) {
+  // decided to make the usePeriodicUpdate a dependency, felt better for tests.
+  const { timeSinceStarted } = usePeriodicUpdate(updateInterval);
+
+  // add a usereducer for the values, uses the `timeSinceStarted` and `updateInterval` to compute values
+  const [values, dispatch] = useReducer(reducer, initialValues);
+
+  useEffect(() => {
+    dispatch({ type: "tick", timeSinceStarted });
+  }, [timeSinceStarted]);
+
+  const updateHunger = useCallback((amount: number) => {
+    dispatch({ type: "updateHunger", amount });
+  }, []);
+
+  const updateHappiness = useCallback((amount: number) => {
+    dispatch({ type: "updateHappiness", amount });
+  }, []);
+
+  const updateSleepiness = useCallback((amount: number) => {
+    dispatch({ type: "updateSleepiness", amount });
+  }, []);
+
   return {
-    hunger: 0,
-    happiness: 0,
-    sleepiness: 0,
+    timeSinceStarted, // remove later, useful for dev
+    values,
+    updateHunger,
+    updateHappiness,
+    updateSleepiness,
   };
 }
